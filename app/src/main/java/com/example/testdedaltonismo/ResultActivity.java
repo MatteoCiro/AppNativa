6f6a7800ea47e06fadc3303dcea2b4e5f7faa39b
MatteoCiro <mateo.agudeloc@upb.edu.co>
2023-04-28 11:40:29 -0500

    Resultados implementados

--- app/src/main/java/com/example/testdedaltonismo/ResultActivity.java
+++ app/src/main/java/com/example/testdedaltonismo/ResultActivity.java
@@ -1,4 +1,18 @@
-package com.example.testdedaltonismo;
+public class ResultActivity extends AppCompatActivity {
 
-public class ResultActivity {
+    TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        txtResult = findViewById(R.id.txt_result);
+
+        int correctAnswers = getIntent().getIntExtra("correctAnswers", 0);
+        int totalQuestions = getIntent().getIntExtra("totalQuestions", 0);
+
+        String resultText = "Resultados: " + correctAnswers + "/" + totalQuestions;
+        txtResult.setText(resultText);
+    }
 }
