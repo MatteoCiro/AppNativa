cf02705f96d5b21e50cdc8daad39251edb57da06
MatteoCiro <mateo.agudeloc@upb.edu.co>
2023-04-28 12:00:27 -0500

    Corrección de todo porque explotó

--- app/src/main/java/com/example/testdedaltonismo/ResultActivity.java
+++ app/src/main/java/com/example/testdedaltonismo/ResultActivity.java
@@ -23,3 +23,4 @@ public class ResultActivity extends AppCompatActivity {
         txtResult.setText(resultText);
     }
 }
+
--- app/src/main/java/com/example/testdedaltonismo/TestActivity.java
+++ app/src/main/java/com/example/testdedaltonismo/TestActivity.java
@@ -74,3 +74,4 @@ public class TestActivity extends AppCompatActivity {
         finish();
     }
 }
+
