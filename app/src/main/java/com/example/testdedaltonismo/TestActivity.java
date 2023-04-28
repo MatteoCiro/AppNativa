e3b400692b76f121b09b3a42a6d7ab27c9ae1a3c
MatteoCiro <mateo.agudeloc@upb.edu.co>
2023-04-28 11:31:18 -0500

    Testxml y actividad test

--- app/src/main/java/com/example/testdedaltonismo/TestActivity.java
+++ app/src/main/java/com/example/testdedaltonismo/TestActivity.java
@@ -1,4 +1,65 @@
-package com.example.testdedaltonismo;
+public class TestActivity extends AppCompatActivity {
 
-public class TestActivity {
+    int[] colors = {
+            Color.RED,
+            Color.GREEN,
+            Color.BLUE,
+            Color.YELLOW,
+            Color.BLACK,
+            Color.WHITE,
+            Color.GRAY,
+            Color.CYAN
+    };
+
+    String[] colorNames = {
+            "Rojo",
+            "Verde",
+            "Azul",
+            "Amarillo",
+            "Negro",
+            "Blanco",
+            "Gris",
+            "Cian"
+    };
+
+    int currentIndex = 0;
+
+    TextView txtColor;
+    TextView txtNumber;
+    Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        txtColor = findViewById(R.id.txt_color);
+        txtNumber = findViewById(R.id.txt_number);
+        btnNext = findViewById(R.id.btn_next);
+
+        showNextColor();
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex++;
+                if (currentIndex < colors.length) {
+                    showNextColor();
+                } else {
+                    finishTest();
+                }
+            }
+        });
+    }
+
+    private void showNextColor() {
+        txtColor.setBackgroundColor(colors[currentIndex]);
+        txtNumber.setText(String.valueOf(currentIndex + 1));
+    }
+
+    private void finishTest() {
+        Intent intent = new Intent(TestActivity.this, ResultActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
