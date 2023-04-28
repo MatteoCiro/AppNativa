da0b3ca0357b5ed96fb5a888dcc03608c7534dd8
MatteoCiro <mateo.agudeloc@upb.edu.co>
2023-04-28 11:06:27 -0500

    Código del main

--- app/src/main/java/com/example/testdedaltonismo/MainActivity.java
+++ app/src/main/java/com/example/testdedaltonismo/MainActivity.java
@@ -1,14 +1,22 @@
-package com.example.testdedaltonismo;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTest;
+    TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTest = findViewById(R.id.btn_test);
+        txtResult = findViewById(R.id.txt_result);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
